4055528ee6859c1b19c3f3a5b47411930f064969
OliverioJunior <oliverio.junior2@gmail.com>
2025-06-04 17:49:25 -0300

    feat(layout): add new Aplication component with theme support

---
+++ src/shared/components/layout/aplication.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { ThemeProvider } from "next-themes";
+import { Header } from "./header";
+import { SideBar } from "./side-bar";
+import { useState } from "react";
+interface IAplication {
+  children: React.ReactNode;
+}
+export const Aplication: React.FC<IAplication> = ({ children }) => {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="dark"
+      enableSystem
+      disableTransitionOnChange
+    >
+      <div className="min-h-screen bg-[var(--background)] text-[var(--foreground)]">
+        <div className="flex">
+          <SideBar
+            sidebarOpen={sidebarOpen}
+            closeSidebar={() => setSidebarOpen(false)}
+          />
+          <div className="flex-1 lg:ml-0">
+            <Header onClick={() => setSidebarOpen(!sidebarOpen)} />
+            {children}
+          </div>
+        </div>
+      </div>
+    </ThemeProvider>
+  );
+};
