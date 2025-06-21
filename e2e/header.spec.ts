f727560211f6fa31d64accc17b32db25c5ffa9d9
OliverioJunior <oliverio.junior2@gmail.com>
2025-06-21 19:34:21 -0300

    feat(test): add playwright e2e testing setup
    - Add playwright configuration and test files
    - Replace jest with playwright in devDependencies
    - Add github workflow for playwright tests
    - Update gitignore for playwright artifacts

---
+++ e2e/header.spec.ts
@@ -0,0 +1,29 @@
+import { test, expect } from "@playwright/test";
+test.describe("Header Component", () => {
+  test.beforeEach(async ({ page }) => {
+    // Navegar para a página que contém o Header
+    await page.goto("/"); // Ajuste para a rota onde o Header está sendo usado
+  });
+  test.describe("Mobile Header", () => {
+    test("deve exibir o header mobile em telas pequenas", async ({ page }) => {
+      // Definir viewport mobile
+
+      await page.setViewportSize({ width: 375, height: 667 });
+
+      // Verificar se o header mobile está visível
+      const mobileHeader = page.getByTestId("mobile-button-menu");
+      await expect(mobileHeader).toBeVisible();
+    });
+    test("não deve exibir o header mobile em telas pequenas", async ({
+      page,
+    }) => {
+      // Definir viewport mobile
+
+      await page.setViewportSize({ width: 1100, height: 800 });
+
+      // Verificar se o header mobile está visível
+      const mobileHeader = page.getByTestId("mobile-button-menu");
+      await expect(mobileHeader).toBeHidden();
+    });
+  });
+});
