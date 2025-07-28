66831019da04235858cf2c7a647a78971ca67c12
OliverioJunior <oliverio.junior2@gmail.com>
2025-07-28 17:57:52 -0300

    test: add coverage config and clear mocks in vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,9 +6,23 @@ export default defineConfig({
   plugins: [react()],
   test: {
     globals: true,
+    clearMocks: true,
     setupFiles: ["./setupTests.ts"],
     environment: "jsdom",
     exclude: ["e2e", "node_modules"],
+    coverage: {
+      exclude: [
+        "**/src/infrastructure/generated/**",
+        "**/src/shared/components/**",
+        ".next",
+        "eslint.config.mjs",
+        "playwright.config.ts",
+        "postcss.config.mjs",
+        "vitest.config.ts",
+        "next-env.d.ts",
+        "next.config.ts",
+      ],
+    },
   },
 
   resolve: {
