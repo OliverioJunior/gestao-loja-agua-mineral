df1e2671d4134ebf048bd65efe50bbf7c8c90c06
OliverioJunior <oliverio.junior2@gmail.com>
2025-07-29 15:51:35 -0300

    test(produto): add error handling test cases for produto module

---
+++ vitest/src/core/produto/produto.errors.test.ts
@@ -0,0 +1,56 @@
+import {
+  ProdutoValidation,
+  ProdutoConflictError,
+  ProdutoNotFoundError,
+} from "@/core/produto/produto.errors";
+describe("ProdutoError", () => {
+  describe("ProdutoValidation", () => {
+    it("should throw ProdutoValidation when nome is required and is empty", () => {
+      expect(() => {
+        throw new ProdutoValidation("nome", "", "nome_required");
+      }).toThrow(ProdutoValidation);
+    });
+    it("should return message when length is less than 2", () => {
+      const produto = new ProdutoValidation("nome", "a", "nome_min_length");
+      expect(produto.message).toBe(
+        "Nome do produto deve ter pelo menos 2 caracteres"
+      );
+    });
+    it("should return message when preco is not positive", () => {
+      const produto = new ProdutoValidation("preco", -1, "preco_positive");
+      expect(produto.message).toBe("Preço deve ser maior que zero");
+    });
+    it("should return message when categoria is invalid", () => {
+      const produto = new ProdutoValidation(
+        "categoria",
+        "invalid",
+        "categoria_invalid"
+      );
+      expect(produto.message).toBe("Categoria inválida");
+    });
+  });
+  describe("ProdutoConflictError", () => {
+    it("should throw ProdutoConflictError when nome is already exists", () => {
+      expect(() => {
+        throw new ProdutoConflictError("nome", "produto");
+      }).toThrow(ProdutoConflictError);
+    });
+    it("should throw ProdutoConflictError when codigo is already exists", () => {
+      expect(() => {
+        throw new ProdutoConflictError("codigo", "123");
+      }).toThrow(ProdutoConflictError);
+    });
+  });
+
+  describe("ProdutoNotFoundError", () => {
+    it("should throw ProdutoNotFoundError when produto is not found", () => {
+      expect(() => {
+        throw new ProdutoNotFoundError("123");
+      }).toThrow(ProdutoNotFoundError);
+    });
+    it("should return message when produto is not found", () => {
+      const produto = new ProdutoNotFoundError("123");
+      expect(produto.message).toBe("Produto não encontrado (ID: 123)");
+    });
+  });
+});
